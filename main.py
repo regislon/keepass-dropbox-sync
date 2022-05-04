bf7e64ecb0a2dd2e3f2b618d2eee191da098f784
Régis Longchamp <regis@lonco.ch>
2022-05-04 14:38:44 +0200

    format

--- main.py
+++ main.py
@@ -2,27 +2,35 @@ import os
 import dropbox
 from pykeepass import PyKeePass
 
-
 ACCESS_TOKEN = '...'
 dbx = dropbox.Dropbox(ACCESS_TOKEN)
 
-def download_dropbox_file(dropbox_file_path : str,local_path :str) :
-    dbx.files_download_to_file(local_path,dropbox_file_path)
+
+def download_dropbox_file(dropbox_file_path: str, local_path: str):
+    """
+    Download file from dropbox
+    """
+    dbx.files_download_to_file(local_path, dropbox_file_path)
 
 
-def upload_dropbox_file(dropbox_file_path : str,local_path :str):
+def upload_dropbox_file(dropbox_file_path: str, local_path: str):
+    """
+    Upload file to dropbox
+    """
     with open(local_path, "rb") as f:
-        response = dbx.files_upload(f.read(),dropbox_file_path)
+        response = dbx.files_upload(f.read(), dropbox_file_path)
     print(response)
 
-def list_files_dropbox_folder(dropbox_folder_path) :
+
+def list_files_dropbox_folder(dropbox_folder_path):
+    """
+    List of file in a dropbox folder
+    """
     list_folder_results = dbx.files_list_folder(dropbox_folder_path)
     files_name = list()
-    for i in list_folder_results.entries :
+    for i in list_folder_results.entries:
         files_name.append(i.name)
     return files_name
-
-
 
 
 # Press the green button in the gutter to run the script.
@@ -36,12 +44,6 @@ if __name__ == '__main__':
 
     local_path = "C://Users//regis//Desktop//test//database_test.kdbx"
 
-
-
-
-
-
-
     # local_path = "C:\\Users\\regis\\Desktop\\test\\database.kdbx"
     #
     # dropbox_file_path = "/KeepPass/database.kdbx"
@@ -50,4 +52,3 @@ if __name__ == '__main__':
     #
     #
     # upload_dropbox_file(dropbox_file_path,local_path)
-
