240828814f0f15876d04149d6e36505b8c28cad2
Régis Lonagchamp <regis@lonco.ch>
2022-05-17 20:16:10 +0200

    load conflict

---
+++ config.py
@@ -0,0 +1,18 @@
+"""
+Dynaconf Settings
+"""
+from dynaconf import Dynaconf, Validator
+
+settings = Dynaconf(
+    envvar_prefix=False,
+    settings_files=[
+        'settings.toml',
+        'secrets.toml',
+    ],
+    validators=[
+        Validator('host', default=False),
+        Validator('apiversion', default='False'),
+        Validator('apikey', default=False),
+
+    ],
+)
--- main.py
+++ main.py
@@ -1,54 +1,57 @@
-import os
-import dropbox
+from os import listdir, path
+
 from pykeepass import PyKeePass
 
-ACCESS_TOKEN = '...'
-dbx = dropbox.Dropbox(ACCESS_TOKEN)
+from config import settings
 
 
-def download_dropbox_file(dropbox_file_path: str, local_path: str):
+def list_of_duplicate(keepass_folder_path: str, kepass_main_db_name: str) -> list:
     """
-    Download file from dropbox
+    This function list all duplicates created in case of conflict in dropbox
     """
-    dbx.files_download_to_file(local_path, dropbox_file_path)
+    duplicates = [f for f in listdir(keepass_folder_path)
+                  if path.isfile(path.join(keepass_folder_path, f))
+                  and f.endswith('.kdbx') and f != kepass_main_db_name]
+    return duplicates
 
 
-def upload_dropbox_file(dropbox_file_path: str, local_path: str):
+def confict_highlights():
     """
-    Upload file to dropbox
+    Load all conflict entries into the conflict group in the main db
     """
-    with open(local_path, "rb") as f:
-        response = dbx.files_upload(f.read(), dropbox_file_path)
-    print(response)
+    keepass_folder_path = settings.keepass_folder_path
+    kepass_main_db_name = settings.kepass_main_db_name
+    keepass_list_duplicate = list_of_duplicate(keepass_folder_path, kepass_main_db_name)
+
+    main_file_path = path.join(keepass_folder_path, kepass_main_db_name)
+    main = PyKeePass(main_file_path, settings.password)
+
+    if not main.find_groups(name='conflict', first=True):
+        main.add_group(main.root_group, 'conflict')
+        main.save()
+
+    conflict_group = main.find_groups(name='conflict', first=True)
+
+    for duplicate_file_name in keepass_list_duplicate:
+        duplicate_file_path = path.join(keepass_folder_path, duplicate_file_name)
+        duplicate = PyKeePass(duplicate_file_path, settings.password)
+        for group in duplicate.groups:
+            for item in group.entries:
+                duplicate_password = item.password
+                duplicate_uuid = item.uuid
+                duplicate_username = item.username
+                duplicate_title = item.title
+                if not main.find_entries(uuid=duplicate_uuid, first=True) \
+                        and not main.find_entries(group=conflict_group, title=duplicate_title, first=True):
+                    if duplicate_password and duplicate_title:
+                        info = f"load {duplicate_title} from {duplicate_file_name}"
+                        print(info)
+                        note = f"imported from {duplicate_file_name}"
+                        main.add_entry(conflict_group, title=duplicate_title, username=duplicate_username,
+                                       password=duplicate_password, notes=note)
+
+    main.save()
 
 
-def list_files_dropbox_folder(dropbox_folder_path):
-    """
-    List of file in a dropbox folder
-    """
-    list_folder_results = dbx.files_list_folder(dropbox_folder_path)
-    files_name = list()
-    for i in list_folder_results.entries:
-        files_name.append(i.name)
-    return files_name
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # dropbox_folder_path = "/KeepPass"
-    # files_name = list_files_dropbox_folder(dropbox_folder_path)
-    # for name in files_name :
-    #     dropbox_file_path = f"{dropbox_folder_path}/{name}"
-    #     local_path = os.path.join("C:\\Users\\regis\\Desktop\\test",name)
-    #     download_dropbox_file(dropbox_file_path, local_path)
-
-    local_path = "C://Users//regis//Desktop//test//database_test.kdbx"
-
-    # local_path = "C:\\Users\\regis\\Desktop\\test\\database.kdbx"
-    #
-    # dropbox_file_path = "/KeepPass/database.kdbx"
-    #
-    # download_dropbox_file(dropbox_file_path,local_path)
-    #
-    #
-    # upload_dropbox_file(dropbox_file_path,local_path)
+    confict_highlights()
